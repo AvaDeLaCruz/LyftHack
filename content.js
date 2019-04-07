a4048b620a07b63161fa1338f92980529858afb3
Ava DeLaCruz <avadelacruz@Avas-MacBook-Pro.local>
2019-04-06 23:27:20 -0700

    less errors boi

--- content.js
+++ content.js
@@ -1,18 +1,32 @@
-var addressText = document.getElementsByClassName('Z0LcW')[0].innerHTML;
+var addressText;
 
-document.getElementsByClassName('Z0LcW')[0].addEventListener("mouseover", function () {
-  document.getElementsByClassName('Z0LcW')[0].style.color = "#EA0B8C";
-  document.getElementsByClassName('Z0LcW')[0].title = "Click here to request your ride!";
+if(window.location.hostname == "www.google.com"){
 
-});
-
-document.getElementsByClassName('Z0LcW')[0].addEventListener("mouseout", function () {
-  document.getElementsByClassName('Z0LcW')[0].style.color = "black";
-});
+  addressText = document.getElementsByClassName('Z0LcW')[0].innerHTML;
 
 
+  document.getElementsByClassName('Z0LcW')[0].addEventListener("mouseover", function () {
+    document.getElementsByClassName('Z0LcW')[0].style.color = "#EA0B8C";
+    document.getElementsByClassName('Z0LcW')[0].title = "Click here to request your ride!";
 
-document.getElementsByClassName('Z0LcW')[0].addEventListener('click', function () {
-  window.location.href = "https://www.lyft.com/fare-estimate";
+  });
 
-});
+  document.getElementsByClassName('Z0LcW')[0].addEventListener("mouseout", function () {
+    document.getElementsByClassName('Z0LcW')[0].style.color = "black";
+  });
+
+
+
+  document.getElementsByClassName('Z0LcW')[0].addEventListener('click', function () {
+    window.location.href = "https://www.lyft.com/fare-estimate";
+
+  });
+
+}
+
+if(window.location.hostname == "www.lyft.com"){
+  console.log(addressText);
+  document.getElementById("destinationInput").value = addressText;
+
+
+}
